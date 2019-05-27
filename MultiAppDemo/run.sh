60264353ced315475d21d26f6db34a778b22367b
Dave Cook <david.cook@bglgroup.co.uk>
2019-05-27 15:30:59 +0100

    Update folder structure to restore github colour coding of dockerfiles

--- MultiAppDemo/run.sh
+++ MultiAppDemo/run.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env bash
 
-docker system prune -f
+docker system prune -f ## clean up any previous/orphaned objects to ensure this run executes cleanly
 
-#eval "$(aws ecr get-login --no-include-email --region us-east-1)"
-
-docker-compose -f ./docker-compose.yml up --build --exit-code-from test
-
-exitCode=$?
-
-docker-compose down --remove-orphans
-
-RED="\033[1;31m"
-GREEN="\033[1;32m"
-
-if [ $exitCode -eq 0 ]
-then
-   echo -e "${GREEN}Tests PASSED. "
-   
-else
-    echo -e "${RED}Tests Failed. "
-fi
-
-exit $exitCode
+docker-compose up --build
