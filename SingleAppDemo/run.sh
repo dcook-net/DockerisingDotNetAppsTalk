c99a3347978e6fd65c94c63a598d0c6cb650c7ab
Dave Cook <david.cook@bglgroup.co.uk>
2019-06-08 20:54:56 +0100

    Updated docker files and launch scripts

--- SingleAppDemo/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t isprime_dotnetcore -f ./Dockerfile_Iterations/v5/Dockerfile .
-docker run -p 9021:9021 isprime_dotnetcore 
---
+++ SingleAppDemo/run_v2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t isprime_dotnetcore:2 -f ./Dockerfile_Iterations/v1/Dockerfile .
+docker run -p 9021:9021 isprime_dotnetcore:2 
