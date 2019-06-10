b000610d2b4192c2bdda23f41ed4476962321718
Dave Cook <david.cook@bglgroup.co.uk>
2019-06-10 11:02:21 +0100

    Update to latest .Net Core SDK and fix issues with bash scripts

--- SingleAppDemo/run_v2.sh
+++ SingleAppDemo/run_v2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t isprime_dotnetcore:2 -f ./Dockerfile_Iterations/v1/Dockerfile .
-docker run -p 9021:9021 isprime_dotnetcore:2 
+docker build -t isprime_dotnetcore:2 -f ./Dockerfile_Iterations/v2/Dockerfile .
+docker run -p 9021:9021 isprime_dotnetcore:2
--- SingleAppDemo/run_v4.sh
+++ SingleAppDemo/run_v4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t isprime_dotnetcore:4 -f ./Dockerfile_Iterations/v1/Dockerfile .
+docker build -t isprime_dotnetcore:4 -f ./Dockerfile_Iterations/v4/Dockerfile .
 docker run -p 9021:9021 isprime_dotnetcore:4
